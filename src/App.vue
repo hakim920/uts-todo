b7ffb4b795d6a1b468b71c55141f33ad9997921e
Rifkiaditia1 <rifkididit205@gmail.com>
2025-05-08 13:23:43 +0700

    Menambahkan struktur dasar web tanpa style

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>To-Do List</h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Tulis tugas..." />
+    <button @click="addTask">Tambah</button>
+
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTask = ref('')
+const tasks = ref([])
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push({ text: newTask.value.trim(), done: false })
+    newTask.value = ''
+  }
+}
+</script>
